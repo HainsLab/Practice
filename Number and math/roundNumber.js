09d739f3e1d7664a147ccda084d66ab6dbe6712b
Muhammad Ihsan <mihsan467@gmail.com>
2023-08-20 21:36:16 +0700

    20 agustus 2023 keep learn

--- Number and math/roundNumber.js
+++ Number and math/roundNumber.js
@@ -1,6 +1,6 @@
-a = 10;
-b = 12.3;
-total = a + b;
-rounded = Math.round(total);
-console.log(rounded)
-document.getElementById("Output").innerHTML = rounded;
+a = 2095;
+b = 799;
+total = Math.round((a + b) * 0.1 ) / 100;
+//rounded = Math.round(total);
+console.log(total)
+document.getElementById("Output").innerHTML = total;
